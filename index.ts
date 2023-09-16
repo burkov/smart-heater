fe0d2c9554e3256c17076649bb8e8ff498b17608
Alexander Burkov <alexander.burkov@jetbrains.com>
2023-09-17 02:52:04 +0300

    Dim in the night

--- index.ts
+++ index.ts
@@ -4,7 +4,9 @@ import {
   compareAsc,
   differenceInMinutes,
   endOfTomorrow,
+  format,
   formatISO,
+  getHours,
   isAfter,
   isEqual,
   parseISO,
@@ -12,10 +14,11 @@ import {
   startOfHour,
   startOfToday,
 } from 'date-fns';
-import { z, ZodError, ZodString } from 'zod';
+import {z, ZodError, ZodString} from 'zod';
 
 //@ts-ignore
-import { GroveLCDRGB } from './grove-lcd';
+import {GroveLCDRGB} from './grove-lcd';
+import os from 'os'
 
 const zodFetch = async <TData>(schema: { parse: (o: any) => TData }, url: string, params?: Record<string, string>) => {
   const urlWithParams = params ? url + '?' + new URLSearchParams(params) : url;
@@ -32,7 +35,7 @@ const zodFetch = async <TData>(schema: { parse: (o: any) => TData }, url: string
 
 const priceSchema = z.object({
   startDate: z
-    .preprocess<ZodString>((e) => `${e}:00+03:00`, z.string().datetime({ offset: true }))
+    .preprocess<ZodString>((e) => `${e}:00+03:00`, z.string().datetime({offset: true}))
     .transform((e) => parseISO(e)),
   value: z.number(),
   unit: z.string(),
@@ -63,7 +66,7 @@ const fetchPrices = async (daysOffset: number = 0, take: number = 12) => {
   if (data.error) throw new Error(`Fortum response contains error: ${JSON.stringify(data)}`);
   const sorted = data.series.sort((a, b) => compareAsc(a.startDate, b.startDate));
   return sorted
-    .filter(({ startDate }) => isAfter(startDate, startFrom) || isEqual(startDate, startFrom))
+    .filter(({startDate}) => isAfter(startDate, startFrom) || isEqual(startDate, startFrom))
     .slice(0, take);
 };
 
@@ -75,7 +78,7 @@ const color = (value: number): [number, number, number] => {
   return [brightness, 0x00, 0x00];
 };
 
-const withUnit = (p?: Price) => (p ? `${p.value.toFixed(2)} ${p.unit}` : '?.?? c/kWh');
+const withUnit = (p?: Price) => (p ? `${p.value.toFixed(2)} c` : '?.?? c');
 
 const stats = (prices: Price[]) => {
   if (prices.length === 0) return undefined;
@@ -86,12 +89,12 @@ const stats = (prices: Price[]) => {
   };
 };
 
-const lcd = new GroveLCDRGB();
-lcd.on();
+const lcd = os.hostname() === 'malina' ? new GroveLCDRGB() : undefined;
+lcd?.on();
 
 const hourly = (func: () => void) => {
   const now = new Date();
-  const nextHour = set(addHours(now, 1), { minutes: 0 });
+  const nextHour = set(addHours(now, 1), {minutes: 0})
   const diff = differenceInMinutes(nextHour, now);
   console.log(`${formatISO(now)} :: Calling func(), next run in ${diff} minutes`);
   try {
@@ -102,23 +105,25 @@ const hourly = (func: () => void) => {
   setTimeout(() => hourly(func), diff * 60 * 1000);
 };
 
+
 hourly(async () => {
+  const hour = getHours(new Date())
   try {
     const series = await fetchPrices();
     const data = stats(series);
+    const ts1 = format(series[0].startDate, 'HH:mm')
+    const ts2 = format(addHours(series[0].startDate, 1), 'HH:mm')
     if (data) {
-      const { now, next } = data;
+      const {now, next} = data;
       const value = series[0].value;
-      let text = `now: ${now}\n`;
-      text += `nxt: ${next ?? '???'}`;
-      lcd.setRGB(...color(value));
-      lcd.setText(text);
+      lcd?.setRGB(...(hour >= 3 && hour <= 8 ? [0, 0, 0] : color(value)));
+      lcd?.setText(`${ts1}: ${now}\n${ts2}: ${next}`);
     }
   } catch (e) {
     console.error(
       `Failed to fetch prices: ${e instanceof ZodError ? JSON.stringify(e.flatten().fieldErrors) : (e as any).message}`,
     );
-    lcd.setRGB(brightness * 2, 0, 0);
-    lcd.setText(`ERROR!`);
+    lcd?.setRGB(brightness * 2, 0, 0);
+    lcd?.setText(`ERROR!`);
   }
 });
